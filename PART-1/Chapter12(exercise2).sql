2f12dd73c2e7c9fbb50fb51f10982d49efdde66b
ha2hi <56156892+ha2hi@users.noreply.github.com>
2021-03-23 17:30:11 +0900

    Create Chapter12(exercise2).sql

---
+++ PART-1/Chapter12(exercise2).sql
@@ -0,0 +1,11 @@
+--Chapter12 실습문제 2
+--고객들에게 단체 이메일을 전송 하고자 한다. CUSTOMER테이블에서 고객의 EMAIL주소를 추출하고, 
+--이메일 형식에 맞지 않는 이메일 주소는 제외시켜라.
+--이메일 형식은 @가 존재해야 하고 @로 시작하지 말아야 하고 @로 끝나지 말아야 한다.
+ select
+ 	email
+ from 
+ 	customer 
+ where email not like '@%'
+ and   email not like '%@'
+ and   email like '%@%';
